15e92670c0f9a26e0015a06673d9fdb0a2989cd5
Fan Dev <134129030+FanDev404@users.noreply.github.com>
2023-06-13 17:25:27 +0700

    Add files via upload

--- system/commands/menu.js
+++ system/commands/menu.js
@@ -31,32 +31,17 @@ module.exports = {
         } else if (sock.pickRandom(["pptx","xlsx","zip","pdf","docx"]) == "docx") {
         var AppType = "application/vnd.openxmlformats-officedocument.wordprocessingml.document"
         } 
-        sock.sendMessage(m.chat, { contextInfo: options, document: fs.readFileSync("./temp/.file"), mimetype: AppType, title : "Footer text", fileLength : 999999999999, pageCount: 100, fileName : botName, caption: menunya + "\n" + fiturnya, headerType: "DOCUMENT", jpegThumbnail: dfrply }, { quoted: setQuoted })
-        setTimeout(() => {
-        sock.sendMessage(m.chat, { audio: { url: "https://raw.githubusercontent.com/Aztecs444/audio/Zeck/sound" + randomNomor(161) + ".mp3" }, mimetype: "audio/mp4", ptt: true })
-        }, 2000)
+        sock.sendMessage(m.chat, { contextInfo: options, document: fs.readFileSync("./temp/Zzzzzzzzzz@4.0.4"), mimetype: AppType, title : "Footer text", fileLength : 999999999999, pageCount: 100, fileName : botName, caption: menunya + "\n" + fiturnya, headerType: "DOCUMENT", jpegThumbnail: dfrply }, { quoted: setQuoted })
         } else if (setMenu == "context") {
         sock.sendMessage(m.chat, { text: menunya + "\n" + fiturnya, contextInfo: options }, { quoted: setQuoted })
-        setTimeout(() => {
-        sock.sendMessage(m.chat, { audio: { url: "https://raw.githubusercontent.com/Aztecs444/audio/Zeck/sound" + randomNomor(161) + ".mp3" }, mimetype: "audio/mp4", ptt: true })
-        }, 2000)
         } else if (setMenu == "viewonce") {
         sock.sendMessage(m.chat, { image: kfrply, caption: menunya + "\n" + fiturnya, viewOnce: true, contextInfo: options }, { quoted: setQuoted })
         } else if (setMenu == "image") {
         sock.sendMessage(m.chat, { image: kfrply, caption: menunya + "\n" + fiturnya, contextInfo: options }, { quoted: setQuoted })
-        setTimeout(() => {
-        sock.sendMessage(m.chat, { audio: { url: "https://raw.githubusercontent.com/Aztecs444/audio/Zeck/sound" + randomNomor(161) + ".mp3" }, mimetype: "audio/mp4", ptt: true })
-        }, 2000)
         } else if (setMenu == "gif") {
         sock.sendMessage(m.chat, { video: pfrply, contextInfo: options, jpegThumbnail: kfrply, caption: menunya + "\n" + fiturnya, gifPlayback: true }, { quoted: setQuoted })
-        setTimeout(() => {
-        sock.sendMessage(m.chat, { audio: { url: "https://raw.githubusercontent.com/Aztecs444/audio/Zeck/sound" + randomNomor(161) + ".mp3" }, mimetype: "audio/mp4", ptt: true })
-        }, 2000)
         } else if (setMenu == "video") {
         sock.sendMessage(m.chat, { video: pfrply, contextInfo: options, jpegThumbnail: kfrply, caption: menunya + "\n" + fiturnya, gifPlayback: false }, { quoted: setQuoted })
-        setTimeout(() => {
-        sock.sendMessage(m.chat, { audio: { url: "https://raw.githubusercontent.com/Aztecs444/audio/Zeck/sound" + randomNomor(161) + ".mp3" }, mimetype: "audio/mp4", ptt: true })
-        }, 2000)
         }
     }
 }
